b0b18db585ff29e571444486d1580611b7a5dacb
kopicoding <adhyc4n@gmail.com>
2021-11-01 10:45:00 +0800

    Add files via upload

---
+++ app/src/main/java/sisfo/mobile/recyclerview/DataAdapter.java
@@ -0,0 +1,53 @@
+package sisfo.mobile.recyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+
+public class DataAdapter extends BaseAdapter {
+
+    //TO DO 1
+
+
+    //TO DO 3
+    //public void setDataFilmsadapter
+
+    //TO DO 4
+    // @Override
+    // public int getCount() {
+    //
+    // }
+    //
+    // @Override
+    // public Object getItem(int position) {
+    //
+    // }
+    //
+    // @Override
+    // public long getItemId(int position) {
+    //
+    // }
+    //
+    //
+    // @Override
+    // public View getView(int position, View convertView, ViewGroup parent) {
+          //TO DO 5
+    // }
+
+    //TO DO 5
+    //private class ViewHolder {
+        //TO DO 6
+
+
+    //}
+
+
+
+}
